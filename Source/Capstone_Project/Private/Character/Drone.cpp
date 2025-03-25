87fc57b1ae47707a604a5ebccc51e71dcf1914f1
Lukasz Dziedziczak <wukash@gmail.com>
2025-03-26 00:03:36 +1030

    Added robot and drone

---
+++ Source/Capstone_Project/Private/Character/Drone.cpp
@@ -0,0 +1,36 @@
+// Written and owned by Lukasz Dziedziczak. Copywrite 2025
+
+
+#include "Character/Drone.h"
+
+// Sets default values
+ADrone::ADrone()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+}
+
+// Called when the game starts or when spawned
+void ADrone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADrone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADrone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Capstone_Project/Public/Character/Drone.h
@@ -0,0 +1,32 @@
+// Written and owned by Lukasz Dziedziczak. Copywrite 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Drone.generated.h"
+
+UCLASS()
+class CAPSTONE_PROJECT_API ADrone : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ADrone();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Mesh;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
